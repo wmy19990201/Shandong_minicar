315f79c2ed2be15507924457eb4131ff36ebd562
wmy19990201 <48408239+wmy19990201@users.noreply.github.com>
2021-10-13 17:04:17 +0800

    Add files via upload

---
+++ function_wmy.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 13:10:31 2021
+
+@author: wang_returns
+"""
+#山东微缩车项目中可能调用的函数
+import numpy as np
+
+l = 45.641#20cm = 45.641 像素
+
+#用纯追踪算法控制小车的舵机转角实现巡线
+#输入量：摄像头坐标系下小车位置坐标（单位：像素）、预瞄点坐标（单位：像素）、小车朝向角（单位：弧度）
+#输出量：小车舵机转角（单位：弧度）
+def line_follower(msr, setpoint, theta_msr):
+    Kp = 1 #P控制器参数（带调节）
+    ##计算预瞄点相对于小车后轮的坐标
+    #输入量：相对于原坐标系的坐标值（二维向量）、输出坐标系相对于原坐标系坐标（二维向量）、输出坐标系相对于原坐标系的旋转角（单位：弧度）
+    #输出量：相对于输出坐标系的坐标值（二维向量）
+    def frame_trans(input_coordinate, relative_coordinate, theta):
+        #计算齐次坐标变换矩阵
+        T = np.array([[np.cos(theta), -np.sin(theta), relative_coordinate[0]],
+                     [np.sin(theta), np.cos(theta), relative_coordinate[1]],
+                     [0 ,              0   ,                  1            ]])
+        
+        
+        input_vector = np.array([[input_coordinate[0]],[input_coordinate[1]], 1])#点在原来坐标系的坐标[x,y,1]^T
+        input_vector = np.array([input_coordinate[0],input_coordinate[1], 1])#点在原来坐标系的坐标[x,y,1]^T
+        output_vector = np.dot(T, input_vector)#求出相对于新坐标系的坐标[x',y',1]^T
+        
+        return [output_vector[0], output_vector[1]]
+        
+        
+    sp_vehicle = frame_trans(msr, setpoint, theta_msr)#得到预瞄点相对于小车后轮的坐标
+    dis2sp = np.sqrt((msr[0]-setpoint[0])**2 + (msr[1]-setpoint[1])**2) #小车中心点到预瞄点的距离
+    err_theta = np.arctan2(sp_vehicle[1],sp_vehicle[0]) #预瞄点与小车中心的连线与小车中轴线之间的夹角
+   
+    #纯追踪算法进行横向控制
+    u = Kp * np.arctan2(2*l*np.sin(err_theta)/dis2sp, 1.0)
+    return u
+
+print(line_follower([1,2],[3,4], 1))
---
+++ 多车.py
@@ -0,0 +1,176 @@
+#摄像头识别二维码并得到二维码位姿
+import numpy as np
+import time
+import cv2
+import cv2.aruco as aruco
+from functions import *
+
+font = cv2.FONT_HERSHEY_SIMPLEX #font for displaying text (below)
+
+from digi.xbee.devices import XBeeDevice
+# pygame用于获取键盘按键信息，控制发射指令
+import time
+# 发送信息途中会出现错误，可能子板不存在（小车没开电）
+from digi.xbee.exception import TransmitException
+
+# xbee主板的com口
+PORT = "COM7"
+# 波特率，主板子板要一致，如果控制多辆小车，建议115200， 9600太小
+BAUD_RATE = 115200
+# xbee的ni，子板要配置，主板不用配置。使用的是186固件，升级过的。
+REMOTE_NODE_ID = ["8"]
+# 创建一个主板对象
+device = XBeeDevice(PORT, BAUD_RATE)
+# 用于存储已经连接上的子板
+end_device = []
+
+device.open()
+xbee_network = device.get_network()
+# remote_device1 = xbee_network.discover_device("2")
+for i in range(1):
+    try:
+        # 用于链接子板
+        tmp = xbee_network.discover_device(REMOTE_NODE_ID[i])
+    except ValueError:
+        tmp = 0
+    end_device.append(tmp)
+    print(end_device)
+
+
+#轨迹规划
+Points = [
+            (15, 180, np.pi / 2),    # 车库入口
+            (58, 55, np.pi / 2),     # 倒车圆弧起点
+            (33, 80, 0),     # 倒车圆弧终点
+            (28, 80, 0),     # 车库
+            (33, 80, 0),     # 离库起点
+            (165, 15, 0),    # 车库出口
+            ]
+
+# STEP 1. 用正弦函数调整位置，准备倒车入库
+y1 = 180 + np.arange(Points[1][1] - Points[0][1],-1)
+x1 = np.zeros_like(y1)
+for i in range(len(x1)):
+    x1[i] = (Points[1][0] + Points[0][0]) / 2 - (Points[1][0] - Points[0][0]) / 2 * np.cos((np.pi/(Points[1][1] - Points[0][1]))*y1[i])
+
+
+# STEP 2. 走圆形，完成倒库动作
+phi2 = np.arange(int(Points[1][2] / np.pi * 180 - 90), int(Points[2][2] / np.pi * 180 - 90), -1)
+#-------- 计算半径 ----------#
+R2 = 25
+#-------- R2即为半径 ---------#
+x2 = np.zeros_like(phi2)
+y2 = np.zeros_like(phi2)
+for i in range(len(phi2)):
+    x2[i] = Points[2][0] + R2 * np.cos(phi2[i] * np.pi / 180)
+    y2[i] = 250 - Points[1][1] - R2 * np.sin(phi2[i] * np.pi / 180)
+
+# STEP 3. 直线倒车进库
+x3 = np.arange(Points[2][0], Points[3][0], -1)
+y3 = 250 - Points[2][1] * np.ones_like(x3)
+
+
+# STEP 4. 直线出库
+x4 = np.arange(Points[3][0], Points[4][0], -1)
+y4 = 250 - Points[3][1] * np.ones_like(x4)
+
+
+# STEP 5. 正弦函数出
+x5 = np.arange(Points[5][0] - Points[4][0]) + Points[4][0]
+y5 = np.zeros_like(x5)
+for i in range(len(x5)):
+    y5[i] = 250 - ((Points[5][1] + Points[4][1]) / 2 - (Points[5][1] - Points[4][1]) / 2 * np.cos((np.pi/(Points[5][0] - Points[4][0]))*x5[i]))
+
+# 选择摄像头的编号
+cap = cv2.VideoCapture(0)
+# 设置960*540,帧率60
+cap.set(3,960)
+cap.set(4,540)
+cap.set(5,60)
+# 添加这句是可以用鼠标拖动弹出的窗体
+cv2.namedWindow('real_img', cv2.WINDOW_NORMAL)
+while(cap.isOpened()):
+    # 读取摄像头的画面
+    ret, frame = cap.read()
+    #图像高度hght与宽度wght
+    hght= frame.shape[0]
+    wght= frame.shape[1]
+    #灰度化
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #设置预定义的字典
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
+    #使用默认值初始化检测器参数
+    parameters =  aruco.DetectorParameters_create()
+    M = calM_3()
+    #透视变换
+    #gray = cv2.warpPerspective(gray,M,(hght, wght))
+    
+    #使用aruco.detectMarkers()函数可以检测到二维码，返回ID和标志板的4个角点坐标
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(frame,aruco_dict,parameters=parameters)
+
+    #若找得到id
+    if ids is not None:
+        for i in range(len(corners)):
+            markerCenter,pose,theta,delta_x,delta_y=location(i,corners,ids)           
+            X = pose[0]
+            Y = pose[1]
+            #画出二维码位置与朝向
+            cv2.arrowedLine(frame, (int(markerCenter[0]), int(markerCenter[1])), (int(markerCenter[0]+delta_x/2), int(markerCenter[1]+delta_y/2)), (255,0,255),5,8,0,0.2)
+
+        #屏幕显示id
+        cv2.putText(frame, "Id: " + str(ids), (0,64), font, 1, (0,255,0),2,cv2.LINE_AA) 
+    
+    #若找不到id
+    else:
+        #屏幕显示没有id
+        cv2.putText(frame, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+    
+    cv2.imshow('real_img', frame)
+
+    #按q退出
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+v = 1
+ii=0
+while ii < len(x1):
+    if X<x1[ii] and Y<y1[ii] :
+        if ii+5 < len(x1):
+            input_delta = np.arctan2(y2[ii+5]-Y,x2[ii+5]-X) - theta
+            print("aaaa")
+        else:
+            input_delta = np.arctan2(y1[-1]-Y,x1[-1]-X) - theta
+    ii += 1
+    input_data = v*100 + input_delta + 90
+    DATA_TO_SEND = str(input_data)
+    if end_device[0]:
+        # 如果连接上就发送信息
+        device.send_data(end_device[0], DATA_TO_SEND)
+        print("bbbbb")
+    else:
+        print("0数据发送失败")
+    time.sleep(0.01)
+    print("第0段完成")
+time.sleep(1)
+
+ii=0
+while ii < len(x2):
+    if X<x2[ii] and Y<y2[ii] :
+        v = 0
+        if ii+5 < len(x2):
+            input_delta = np.arctan2(y2[ii+5]-Y,x2[ii+5]-X) - theta
+        else:
+            input_delta = np.arctan2(y2[-1]-Y,x2[-1]-X) - theta
+    ii += 1
+    input_data = v*100 - input_delta + 90
+    DATA_TO_SEND = str(input_data)
+    if end_device[0]:
+        # 如果连接上就发送信息
+        device.send_data(end_device[0], DATA_TO_SEND)
+    else:
+        print("0数据发送失败")
+    time.sleep(0.01)
+    print("第0段完成")
+
+cap.release()
+cv2.destroyAllWindows()
